87ddf3d8b97d24d86a62d9b67c7e9f24f0f03a16
Jason Tham <jtham@sbcglobal.net>
2016-06-28 23:31:18 -0500

    more modal stylings

--- src/client/containers/modal.jsx
+++ src/client/containers/modal.jsx
@@ -9,7 +9,10 @@ class Modal extends React.Component {
       <div className="row" id="modalContainer">
         {this.props.recipients.map((recipient) =>
           <div className="three columns modalBox">
-            <div className="modalHeader">{recipient}</div>
+            <div className="modalHeader twelve columns">{recipient}</div>
+            <div className="privateMessage twelve columns">
+              <input type="textarea" className="pmTextArea twelve columns" />
+            </div>
           </div>
         )}
       </div>
